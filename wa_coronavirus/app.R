f0c9b3e4cb377e6153ea1ec752bd68eaa34a4146
Emily Linebarger <23664308+kathrynline@users.noreply.github.com>
2020-03-23 09:50:11 -0700

    Update app.R

--- wa_coronavirus/app.R
+++ wa_coronavirus/app.R
@@ -17,11 +17,10 @@ ui <- fluidPage(
     titlePanel("COVID-19 in Washington State"),
     
     # Sidebar with a slider input for number of bins 
-    # sidebarPanel(
-    # selectInput('Select graph view', 'scale',
-    #             c('Normal scale'='norm', 'Log Scale'='log'),
-    #             plotOutput("plot1", click = "plot_click"))
-    # ), 
+    sidebarPanel(
+        radioButtons('scale', 'Select graph view',
+                c('Linear scale'='norm', 'Log Scale'='log'),)
+    ),
     
     # Show a plot of the generated distribution
     mainPanel(
@@ -34,12 +33,26 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
+    max_date = Sys.Date() # Set to today's date 
+    labels = data.table(lab = c('total_cases', 'total_deaths', 'total_tests_to_date'), 
+                        date=rep(max_date, 3), 
+                        value=as.numeric(c(dt[date==max_date, .(total_cases, total_deaths, total_tests_to_date)])))
+    
     output$plot1 <- renderPlot({
-        ggplot(dt, aes(x=date)) + 
+       p =  ggplot(dt, aes(x=date)) + 
             geom_point(aes(y=total_cases), color="red") + 
+            geom_line(aes(y=total_cases), color="red") + 
+            geom_point(aes(y=total_deaths), color="blue") + 
+            geom_line(aes(y=total_deaths), color="blue") + 
+            geom_point(aes(y=total_tests_to_date), color="green") + 
+            geom_line(aes(y=total_tests_to_date), color="green") + 
+            geom_text(data=labels, aes(x=date, y=value, label=lab)) + 
             theme_bw(base_size=16) + 
-            scale_y_log10() + 
             labs(x="Date", y="")
+            if (input$scale =='log'){
+                p = p + scale_y_log10() 
+            }
+       p 
     })
     
     output$info <- renderText({
